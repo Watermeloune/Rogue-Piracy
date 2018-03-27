ebddf7fda13011e0b597e3730974ccb39c7b07a3
DARWISH JZAERLY <basel.darwish-jzaerly@etu.univ-lyon1.fr>
2018-03-27 11:01:04 +0200

    bateau

---
+++ src/Bateau.h
@@ -0,0 +1,22 @@
+#ifndef BATEAU_H
+#define BATEAU_H
+
+
+class Bateau
+{
+    public:
+        Bateau();
+        ~Bateau();
+
+
+    private:
+
+        int PV;
+        int capacite_cargo ;
+        int capacite_equipage ;
+        
+        
+};
+
+#endif // BATEAU_H
+
